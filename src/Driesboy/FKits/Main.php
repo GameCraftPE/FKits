9eb37849112a26581ee641899ac7122c7733fa91
Driesboy <coenendries3@gmail.com>
2017-05-31 07:51:53 +0200

    Lineup

--- src/Driesboy/FKits/Main.php
+++ src/Driesboy/FKits/Main.php
@@ -1,7 +1,7 @@
 <?php
 /*
-	License Text
- */
+License Text
+*/
 namespace Driesboy\FKits;
 
 use pocketmine\command\Command;
@@ -19,8 +19,98 @@ class Main extends PluginBase {
 			$ce = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
 			$item = Item::get(Item::DIAMOND_SWORD);
 			case "aerial":
+			if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
+				$enchant = $ce->addEnchantment($item, "aerial", 1, $sender, null, null, $check, false);
+				$sender->getInventory()->addItem($enchant);
+			}else{
+				switch ($r) {
+					case EconomyAPI::RET_INVALID:
+					# Invalid $amount
+					break;
+					case EconomyAPI::RET_CANCELLED:
+					# Transaction was cancelled for some reason :/
+					break;
+					case EconomyAPI::RET_NO_ACCOUNT:
+					# Player wasn't recognised by EconomyAPI aka. not registered
+					break;
+				}
+			}
+			break;
+			case "blind":
+			if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
+				$enchant = $ce->addEnchantment($item, "blind", 1, $sender, null, null, $check, false);
+				$sender->getInventory()->addItem($enchant);
+			}else{
+				switch ($r) {
+					case EconomyAPI::RET_INVALID:
+					# Invalid $amount
+					break;
+					case EconomyAPI::RET_CANCELLED:
+					# Transaction was cancelled for some reason :/
+					break;
+					case EconomyAPI::RET_NO_ACCOUNT:
+					# Player wasn't recognised by EconomyAPI aka. not registered
+					break;
+				}
+			}
+			break;
+			case "charge":
+			if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
+				$enchant = $ce->addEnchantment($item, "charge", 1, $sender, null, null, $check, false);
+				$sender->getInventory()->addItem($enchant);
+			}else{
+				switch ($r) {
+					case EconomyAPI::RET_INVALID:
+					# Invalid $amount
+					break;
+					case EconomyAPI::RET_CANCELLED:
+					# Transaction was cancelled for some reason :/
+					break;
+					case EconomyAPI::RET_NO_ACCOUNT:
+					# Player wasn't recognised by EconomyAPI aka. not registered
+					break;
+				}
+			}
+			break;
+			case "cripple":
+			if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
+				$enchant = $ce->addEnchantment($item, "cripple", 1, $sender, null, null, $check, false);
+				$sender->getInventory()->addItem($enchant);
+			}else{
+				switch ($r) {
+					case EconomyAPI::RET_INVALID:
+					# Invalid $amount
+					break;
+					case EconomyAPI::RET_CANCELLED:
+					# Transaction was cancelled for some reason :/
+					break;
+					case EconomyAPI::RET_NO_ACCOUNT:
+					# Player wasn't recognised by EconomyAPI aka. not registered
+					break;
+				}
+			}
+			break;
+			case "deathbringer":
+			if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
+				$enchant = $ce->addEnchantment($item, "deathbringer", 1, $sender, null, null, $check, false);
+				$sender->getInventory()->addItem($enchant);
+			}else{
+				switch ($r) {
+					case EconomyAPI::RET_INVALID:
+					# Invalid $amount
+					break;
+					case EconomyAPI::RET_CANCELLED:
+					# Transaction was cancelled for some reason :/
+					break;
+					case EconomyAPI::RET_NO_ACCOUNT:
+					# Player wasn't recognised by EconomyAPI aka. not registered
+					break;
+				}
+			}
+			break;
+			case "lifesteal":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-					$enchant = $ce->addEnchantment($item, "aerial", 1, $sender, null, null, $check, false);
+					$enchant = $ce->addEnchantment($item, "lifesteal", 1, $sender, null, null, $check, false);
 					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
@@ -35,11 +125,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "blind":
+				break;
+				case "poison":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "blind", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "poison", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -53,11 +143,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "charge":
+				break;
+				case "vampire":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-				  $enchant = $ce->addEnchantment($item, "charge", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "vampire", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -71,11 +161,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "cripple":
+				break;
+				case "wither":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-				  $enchant = $ce->addEnchantment($item, "cripple", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "wither", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -89,11 +179,12 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "deathbringer":
+				break;
+				$item = Item::get(Item::DIAMOND_PICKAXE);
+				case "smelting":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "deathbringer", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "smelting", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -107,11 +198,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "lifesteal":
+				break;
+				case "telepathy":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "lifesteal", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "telepathy", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -125,11 +216,12 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "poison":
+				break;
+				$item = Item::get(Item::DIAMOND_AXE);
+				case "lumberjack":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "poison", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "lumberjack", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -143,11 +235,12 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "vampire":
+				break;
+				$item = Item::get(Item::DIAMOND_CHESTPLATE);
+				case "berserker":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "vampire", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "berserker", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -161,11 +254,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "wither":
+				break;
+				case "frozen":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "wither", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "frozen", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -179,12 +272,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			$item = Item::get(Item::DIAMOND_PICKAXE);
-			case "smelting":
+				break;
+				case "hardened":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "smelting", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "hardened", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -198,11 +290,11 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "telepathy":
+				break;
+				case "molten":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "telepathy", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "molten", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -216,100 +308,8 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			$item = Item::get(Item::DIAMOND_AXE);
-			case "lumberjack":
-				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "lumberjack", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
-				}else{
-					switch ($r) {
-						case EconomyAPI::RET_INVALID:
-						# Invalid $amount
-						break;
-						case EconomyAPI::RET_CANCELLED:
-						# Transaction was cancelled for some reason :/
-						break;
-						case EconomyAPI::RET_NO_ACCOUNT:
-						# Player wasn't recognised by EconomyAPI aka. not registered
-						break;
-					}
-				}
-			break;
-			$item = Item::get(Item::DIAMOND_CHESTPLATE);
-			case "berserker":
-				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "berserker", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
-				}else{
-					switch ($r) {
-						case EconomyAPI::RET_INVALID:
-						# Invalid $amount
-						break;
-						case EconomyAPI::RET_CANCELLED:
-						# Transaction was cancelled for some reason :/
-						break;
-						case EconomyAPI::RET_NO_ACCOUNT:
-						# Player wasn't recognised by EconomyAPI aka. not registered
-						break;
-					}
-				}
-			break;
-			case "frozen":
-				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "frozen", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
-				}else{
-					switch ($r) {
-						case EconomyAPI::RET_INVALID:
-						# Invalid $amount
-						break;
-						case EconomyAPI::RET_CANCELLED:
-						# Transaction was cancelled for some reason :/
-						break;
-						case EconomyAPI::RET_NO_ACCOUNT:
-						# Player wasn't recognised by EconomyAPI aka. not registered
-						break;
-					}
-				}
-			break;
-			case "hardened":
-				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-			  	$enchant = $ce->addEnchantment($item, "hardened", 1, $sender, null, null, $check, false);
-			  	$sender->getInventory()->addItem($enchant);
-				}else{
-					switch ($r) {
-						case EconomyAPI::RET_INVALID:
-						# Invalid $amount
-						break;
-						case EconomyAPI::RET_CANCELLED:
-						# Transaction was cancelled for some reason :/
-						break;
-						case EconomyAPI::RET_NO_ACCOUNT:
-						# Player wasn't recognised by EconomyAPI aka. not registered
-						break;
-					}
-				}
-			break;
-			case "molten":
-				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-				  $enchant = $ce->addEnchantment($item, "molten", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
-				}else{
-					switch ($r) {
-						case EconomyAPI::RET_INVALID:
-						# Invalid $amount
-						break;
-						case EconomyAPI::RET_CANCELLED:
-						# Transaction was cancelled for some reason :/
-						break;
-						case EconomyAPI::RET_NO_ACCOUNT:
-						# Player wasn't recognised by EconomyAPI aka. not registered
-						break;
-					}
-				}
-			break;
-			case "selfdestruct":
+				break;
+				case "selfdestruct":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
 					$enchant = $ce->addEnchantment($item, "selfdestruct", 1, $sender, null, null, $check, false);
 					$sender->getInventory()->addItem($enchant);
@@ -326,21 +326,21 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "shrink":
+				break;
+				case "shrink":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
-				  $item = Item::get(Item::DIAMOND_HELMET);
-				  $enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$item = Item::get(Item::DIAMOND_HELMET);
+					$enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 					$item = Item::get(Item::DIAMOND_CHESTPLATE);
-				  $enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 					$item = Item::get(Item::DIAMOND_LEGGINGS);
-				  $enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 					$item = Item::get(Item::DIAMOND_BOOTS);
-				  $enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
-				  $sender->getInventory()->addItem($enchant);
+					$enchant = $ce->addEnchantment($item, "shrink", 1, $sender, null, null, $check, false);
+					$sender->getInventory()->addItem($enchant);
 				}else{
 					switch ($r) {
 						case EconomyAPI::RET_INVALID:
@@ -354,9 +354,9 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			$item = Item::get(Item::DIAMOND_BOOTS);
-			case "gears":
+				break;
+				$item = Item::get(Item::DIAMOND_BOOTS);
+				case "gears":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
 					$enchant = $ce->addEnchantment($item, "gears", 1, $sender, null, null, $check, false);
 					$sender->getInventory()->addItem($enchant);
@@ -373,8 +373,8 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
-			case "springs":
+				break;
+				case "springs":
 				if ($r = EconomyAPI::getInstance()->reduceMoney($sender, 1500)) {
 					$enchant = $ce->addEnchantment($item, "springs", 1, $sender, null, null, $check, false);
 					$sender->getInventory()->addItem($enchant);
@@ -391,7 +391,7 @@ class Main extends PluginBase {
 						break;
 					}
 				}
-			break;
+				break;
+			}
 		}
 	}
-}
